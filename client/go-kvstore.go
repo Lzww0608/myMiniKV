955839e91756ef4125b8d02fc5f0961aecf9bbb0
Lzww <63537700+Lzww0608@users.noreply.github.com>
2024-04-24 15:59:15 +0800

    Update go-kvstore.go

--- client/go-kvstore.go
+++ client/go-kvstore.go
@@ -17,7 +17,7 @@ func main() {
 	}
 	defer conn.Close()
 
-	message := "GET Name"
+	message := "SET Name Lzww"
 	_, err = conn.Write([]byte(message))
 	if err != nil {
 		fmt.Println("send failed: ", err)
@@ -35,8 +35,34 @@ func main() {
 
 	response := string(buffer[:length])
 	fmt.Printf("recv msg: %s\n", response)
+
+// -----------------------------------------------------------------
+	
+	conn, err = net.Dial("tcp", "127.0.0.1:9096")
+	if err != nil {
+		fmt.Println("connect failed: ", err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	message = "GET Name"
+	_, err = conn.Write([]byte(message))
+	if err != nil {
+		fmt.Println("send failed: ", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("send msg: %s\n", message)
+
+	buffer = make([]byte, 1024)
+	length, err = conn.Read(buffer)
+	if err != nil {
+		fmt.Println("recv failed: ", err)
+		os.Exit(1)
+	}
+
+	response = string(buffer[:length])
+	fmt.Printf("recv msg: %s\n", response)
+
 }
-
-
-
 
